0cd558516452c5137f707030ac094d84d35a4f67
ranjith <cranjithrcb@gmail.com>
2024-06-04 22:41:39 +0530

    first commit

---
+++ src/nodes.js
@@ -0,0 +1,22 @@
+const initialNodes = [
+    {
+      id: '1',
+      type: 'input',
+      data: { label: 'Input Node' },
+      position: { x: 250, y: 25 },
+    },
+    {
+      id: '2',
+      data: { label: <div>Default Node</div> },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: '3',
+      type: 'output',
+      data: { label: 'Output Node' },
+      position: { x: 250, y: 250 },
+    },
+  ];
+  
+  export default initialNodes;
+  
